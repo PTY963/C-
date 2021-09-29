71d9745e2fbe6e83c8e56041263b61d9fc9def2b
PTY963 <2924853846@qq.com>
2021-09-29 21:17:42 +0800

    auto、范围for、nullptr

--- test-9-28/test-9-28/test.cpp
+++ test-9-28/test-9-28/test.cpp
@@ -48,3 +48,83 @@ using namespace std;
 //3、不可以调试
 
 //内联函数就是来解决宏的缺点的
+
+
+//int main()
+//{
+//	int a = 1;
+//	char c = 'a';
+//
+//	//通过右边赋值对象，自动推导变量类型
+//	//类型太复杂太长，auto自动推导，简化代码
+//	//缺点：牺牲了代码的可读性
+//	auto d = a;
+//	auto b = c;
+//
+//	//查看auto变量d和b的类型
+//	cout << typeid(d).name() << endl;
+//	cout << typeid(b).name() << endl;
+//	return 0;
+//}
+
+//int main()
+//{
+//	/*int x = 10;
+//	auto a = &x;//auto正常推导
+//	auto* b = &x;//显式的说明是指针类型
+//	auto& c = x;
+//
+//	cout << typeid(a).name() << endl;
+//	cout << typeid(b).name() << endl;
+//	cout << typeid(c).name() << endl;*/
+//
+//	//C++11提供了一种新的访问数组的方式 -- 范围for
+//	//自动依次取数组中的值赋值给e,自动判断结束
+//	int array[] = { 1,2,3,4,5 };
+//	//array数组中的值依次拷贝给e,所以e的改变并不会影响数组
+//	//for (auto e : array)
+//	//{
+//	//	e *= 2;
+//	//}
+//	//for (auto e : array)
+//	//{
+//	//	cout << e << " ";//1,2,3,4,5
+//	//}
+//
+//	for (auto& e : array)
+//	{
+//		e *= 2;
+//	}
+//	for (auto e : array)
+//	{
+//		cout << e << " ";//2,4,6,8,10
+//	}
+//	cout << endl;
+//	
+//	
+//	return 0;
+//}
+
+
+void Fun(int a)
+{
+	cout << "int" << endl;
+}
+void Fun(int* a)
+{
+	cout << "int*" << endl;
+}
+int main()
+{
+	//C++98的用法
+	int* p1 = NULL;
+	//#define NULL 0;
+	int* p3 = 0;
+
+	//C++11
+	int* p2 = nullptr;
+	Fun(0);//int，0被当做字面常量
+	Fun(NULL);//int
+	//原因就在于C++中#define NULL 0;
+	return 0;
+}
